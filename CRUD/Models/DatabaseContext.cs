2b7de84df82215372de78312736dc163dc2ad94d
siddhantparekh7 <siddhantparekh7@gmail.com>
2023-09-03 19:04:45 +0530

    feat: Add CRUD operations
    Basic CRUD operations are working

---
+++ CRUD/Models/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Models
+{
+    public class DatabaseContext: DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext>options) 
+            : base(options)    
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ CRUD/Services/PersonService.cs
@@ -0,0 +1,50 @@
+﻿using CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Services
+{
+    public class PersonService
+    {
+        private readonly DatabaseContext _databaseContext;
+        public PersonService(DatabaseContext databaseContext) 
+        {          
+            _databaseContext = databaseContext;
+        }
+
+        //Get all records
+        public async Task<List<Person>> GetAllPerson()
+        {
+            return await _databaseContext.Persons.ToListAsync();
+        }
+
+        //Add new record
+        public async Task<bool> AddNewPerson(Person person)
+        {
+            await _databaseContext.Persons.AddAsync(person);
+            await _databaseContext.SaveChangesAsync();  
+            return true;
+        }
+
+        //Get Person By Id
+        public async Task<Person> GetPersonById(int id)
+        {
+            Person person = await _databaseContext.Persons.FirstOrDefaultAsync(x => x.Id == id);
+            return person;
+        }
+
+        //Update Person Data
+        public async Task<bool> UpdatePersonDetails(Person person)
+        {
+            _databaseContext.Persons.Update(person);
+            await _databaseContext.SaveChangesAsync(true);
+            return true;
+        }
+        public async Task<bool> DeletePerson(Person person)
+        {
+            _databaseContext.Persons.Remove(person);
+            await _databaseContext.SaveChangesAsync(true);
+            return true;
+        }
+
+    }
+}
